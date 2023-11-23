918ca6a98888d5045a0f6d9bf76c6e81a91560e4
Maxime Organi <maxime.organi@gmail.com>
2023-11-23 14:12:43 +0100

    Add Skeleton component and loading state to Sidebar

--- src/components/Charts/StackedBarChart.tsx
+++ src/components/Charts/StackedBarChart.tsx
@@ -3,128 +3,116 @@ import { Chart as ChartJS, registerables } from "chart.js";
 import { Chart } from "react-chartjs-2";
 import zoomPlugin from "chartjs-plugin-zoom";
 import * as dayjs from "dayjs";
-import { DatePickerWithRange } from "../DateRangePicker";
-import { ResetButton } from "../ResetButton";
-import { TemporalAggregationSelector } from "../TemporalAggregationSelector";
 import { useProjects } from "@/lib/react-query/queries";
 import { useDataParams } from "@/context/DataParamsProvider";
+import { Skeleton } from "../ui/skeleton";
 
 ChartJS.register(...registerables, zoomPlugin);
 
 export const StackedBarChart = () => {
   const { currentBuilding } = useProjects();
-  const { data, setDateRangeFilter, selectedDateRange, selectableDateRange } =
-    useDataContext();
+  const { data, isLoading } = useDataContext();
   const {
-    temporalAggregation: [selectedTemporalAggregation],
+    temporalAggregation: { selectedTemporalAggregation },
   } = useDataParams();
-
-  if (!data?.labels?.length) {
-    return null;
-  }
 
   return (
     <div>
-      {!!selectableDateRange && (
-        <div className="flex justify-between">
-          <DatePickerWithRange
-            selectableDateRange={selectableDateRange}
-            onChange={setDateRangeFilter}
-            selectedDateRange={selectedDateRange}
-          />
-          <TemporalAggregationSelector />
-          <ResetButton />
-        </div>
-      )}
-      <Chart
-        type="bar"
-        options={{
-          plugins: {
-            legend: {
-              position: "bottom",
-            },
-            zoom: {
+      {isLoading || data.labels.length === 0 ? (
+        <Skeleton className="w-full h-[360px] mt-2" />
+      ) : (
+        <Chart
+          className="mt-2"
+          type="bar"
+          options={{
+            plugins: {
+              legend: {
+                position: "bottom",
+              },
               zoom: {
-                wheel: {
-                  enabled: true,
-                },
-                pinch: {
-                  enabled: true,
-                },
-                mode: "x",
-              },
-            },
-            title: {
-              display: true,
-              text: `Consommation ${currentBuilding?.name}`,
-            },
-            tooltip: {
-              callbacks: {
-                title: function (context) {
-                  const date = dayjs(context[0].label).tz(
-                    currentBuilding?.timezone
-                  );
-                  switch (selectedTemporalAggregation) {
-                    case "day":
-                      return date.format("DD/MM/YYYY");
-                    case "week":
-                      return `w${date.week()} / ${date.format("YYYY")}`;
-                    case "month":
-                      return date.format("MM/YYYY");
-                    case "year":
-                      return date.format("YYYY");
-                  }
-                },
-                label: function (context) {
-                  let label = context.dataset.label || "";
-                  if (label) {
-                    label += ": ";
-                  }
-                  if (context.parsed.x !== null) {
-                    label += context.dataset.tooltip[context.parsed.x] + " MWh";
-                  }
-                  return label;
+                zoom: {
+                  wheel: {
+                    enabled: true,
+                  },
+                  pinch: {
+                    enabled: true,
+                  },
+                  mode: "x",
                 },
               },
+              title: {
+                display: true,
+                text: `Consommation ${currentBuilding?.name}`,
+              },
+              tooltip: {
+                callbacks: {
+                  title: function (context) {
+                    const date = dayjs(context[0].label).tz(
+                      currentBuilding?.timezone
+                    );
+                    switch (selectedTemporalAggregation) {
+                      case "day":
+                        return date.format("DD/MM/YYYY");
+                      case "week":
+                        return `w${date.week()} / ${date.format("YYYY")}`;
+                      case "month":
+                        return date.format("MM/YYYY");
+                      case "year":
+                        return date.format("YYYY");
+                    }
+                  },
+                  label: function (context) {
+                    let label = context.dataset.label || "";
+                    if (label) {
+                      label += ": ";
+                    }
+                    if (context.parsed.x !== null) {
+                      label +=
+                        context.dataset.tooltip[context.parsed.x] + " MWh";
+                    }
+                    return label;
+                  },
+                },
+              },
             },
-          },
-          responsive: true,
-          scales: {
-            x: {
-              stacked: true,
-              ticks: {
-                // Include a dollar sign in the ticks
-                callback: function (value, index, ticks) {
-                  const date = dayjs(data?.labels?.[value]).tz(
-                    currentBuilding?.timezone
-                  );
+            responsive: true,
+            scales: {
+              x: {
+                stacked: true,
+                ticks: {
+                  // Include a dollar sign in the ticks
+                  callback: function (value, index, ticks) {
+                    const date = dayjs(data?.labels?.[value]).tz(
+                      currentBuilding?.timezone
+                    );
 
-                  switch (selectedTemporalAggregation) {
-                    case "day":
-                      return date.format("DD/MM/YYYY");
-                    case "week":
-                      return `w${date.week()} / ${date.format("YYYY")}`;
-                    case "month":
-                      return date.format("MM/YYYY");
-                    case "year":
-                      return date.format("YYYY");
-                  }
+                    switch (selectedTemporalAggregation) {
+                      case "day":
+                        return date.format("DD/MM/YYYY");
+                      case "week":
+                        return `w${date.week()} / ${date.format("YYYY")}`;
+                      case "month":
+                        return date.format("MM/YYYY");
+                      case "year":
+                        return date.format("YYYY");
+                    }
+                  },
+                },
+              },
+              y: {
+                stacked: true,
+                ticks: {
+                  callback: function (value, index, values) {
+                    // This function will be called for each tick
+                    return value + " Mwh"; // Replace this with your own logic
+                  },
                 },
               },
             },
-            y: {
-              stacked: true,
-              ticks: {
-                callback: function (value, index, values) {
-                  // This function will be called for each tick
-                  return value + " Mwh"; // Replace this with your own logic
-                },
-              },
-            },
-          },
-        }}
-        data={data}
-      />
+          }}
+          data={data}
+        />
+      )}
     </div>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,15 +2,24 @@ import { Building } from "lucide-react";
 import { Button } from "./ui/button";
 import { ScrollArea } from "./ui/scroll-area";
 import { Link, useLocation } from "react-router-dom";
+import { IBuiding } from "@/lib/types";
+import { Skeleton } from "./ui/skeleton";
 
-type IBuiding = {
-  uuid: string;
-  name: string;
-  timezone: string;
-};
+const ButtonSkeleton = () => (
+  <Skeleton className="flex items-center w-auto animate-pulse">
+    <Button variant={"ghost"} className="w-full justify-start font-normal">
+      <div className=" bg-slate-200 h-[24px] w-[24px] mx-1" />
+      <div className="bg-slate-200  h-4 w-[200px]" />
+    </Button>
+  </Skeleton>
+);
 
-export const Sidebar: React.FC<{ buildings: IBuiding[] }> = ({ buildings }) => {
+export const Sidebar: React.FC<{
+  buildings: IBuiding[];
+  isLoading: boolean;
+}> = ({ buildings, isLoading }) => {
   const location = useLocation();
+
   return (
     <div className="space-y-4 py-4">
       <div className="py-2">
@@ -19,20 +28,24 @@ export const Sidebar: React.FC<{ buildings: IBuiding[] }> = ({ buildings }) => {
         </h2>
         <ScrollArea className="h-[300px] px-1">
           <div className="space-y-1 p-2">
-            {buildings?.map((building) => {
-              const currentBuilding = location.pathname === "/" + building.uuid;
-              return (
-                <Link to={"/" + building.uuid} key={`${building.uuid}`}>
-                  <Button
-                    variant={currentBuilding ? "default" : "ghost"}
-                    className="w-full justify-start font-normal"
-                  >
-                    <Building />
-                    {building.name}
-                  </Button>
-                </Link>
-              );
-            })}
+            {isLoading &&
+              new Array(3).fill(0).map((_, i) => <ButtonSkeleton key={i} />)}
+            {!isLoading &&
+              buildings?.map((building) => {
+                const currentBuilding =
+                  location.pathname === "/" + building.uuid;
+                return (
+                  <Link to={"/" + building.uuid} key={`${building.uuid}`}>
+                    <Button
+                      variant={currentBuilding ? "default" : "ghost"}
+                      className="w-full justify-start font-normal"
+                    >
+                      <Building />
+                      {building.name}
+                    </Button>
+                  </Link>
+                );
+              })}
           </div>
         </ScrollArea>
       </div>
