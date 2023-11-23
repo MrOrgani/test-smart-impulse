9a5aa1e0657bab7ec5b717fe57f2e90d6658a641
Maxime Organi <maxime.organi@gmail.com>
2023-11-23 12:52:49 +0100

    Refactor Sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,32 +12,29 @@ type IBuiding = {
 export const Sidebar: React.FC<{ buildings: IBuiding[] }> = ({ buildings }) => {
   const location = useLocation();
   return (
-    <div>
-      <div className="space-y-4 py-4">
-        <div className="py-2">
-          <h2 className="relative px-7 text-lg font-semibold tracking-tight">
-            Batiments
-          </h2>
-          <ScrollArea className="h-[300px] px-1">
-            <div className="space-y-1 p-2">
-              {buildings?.map((building) => {
-                const currentBuilding =
-                  location.pathname === "/" + building.uuid;
-                return (
-                  <Link to={"/" + building.uuid} key={`${building.uuid}`}>
-                    <Button
-                      variant={currentBuilding ? "default" : "ghost"}
-                      className="w-full justify-start font-normal"
-                    >
-                      <Building />
-                      {building.name}
-                    </Button>
-                  </Link>
-                );
-              })}
-            </div>
-          </ScrollArea>
-        </div>
+    <div className="space-y-4 py-4">
+      <div className="py-2">
+        <h2 className="relative px-7 text-lg font-semibold tracking-tight">
+          Batiments
+        </h2>
+        <ScrollArea className="h-[300px] px-1">
+          <div className="space-y-1 p-2">
+            {buildings?.map((building) => {
+              const currentBuilding = location.pathname === "/" + building.uuid;
+              return (
+                <Link to={"/" + building.uuid} key={`${building.uuid}`}>
+                  <Button
+                    variant={currentBuilding ? "default" : "ghost"}
+                    className="w-full justify-start font-normal"
+                  >
+                    <Building />
+                    {building.name}
+                  </Button>
+                </Link>
+              );
+            })}
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
