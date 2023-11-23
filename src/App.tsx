9d75989cc8d20253a38b902b530db2036872bf1c
Maxime Organi <maxime.organi@gmail.com>
2023-11-23 09:38:18 +0100

    Refactor App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,29 @@ function App() {
   const { buildings } = useProjects();
 
   return (
-    <DataContextProvider>
-      <div
-        className={
-          "container flex items-center justify-center mx-auto my-auto h-screen"
-        }
-      >
-        <div className={"h-3/4 bg-bg-300 rounded-md"}>
-          <Header />
-          <div className="border-t">
-            <div className="">
-              <div className="grid lg:grid-cols-5">
-                <Sidebar buildings={buildings} />
-                <div className="col-span-3 lg:col-span-4 lg:border-l">
-                  <div className="h-full px-4 py-6 lg:px-8">
+    <div
+      className={
+        "container flex items-center justify-center mx-auto my-auto h-screen"
+      }
+    >
+      <div className={"h-3/4 bg-bg-300 rounded-md"}>
+        <Header />
+        <div className="border-t">
+          <div className="">
+            <div className="grid lg:grid-cols-5">
+              <Sidebar buildings={buildings} />
+              <div className="col-span-3 lg:col-span-4 lg:border-l">
+                <div className="h-full px-4 py-6 lg:px-8">
+                  <DataContextProvider>
                     <Outlet />
-                  </div>
+                  </DataContextProvider>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </DataContextProvider>
+    </div>
   );
 }
 
