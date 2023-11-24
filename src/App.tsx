3727833a3fb0b141b71e5c2a1815e70a5d3869b4
Maxime Organi <maxime.organi@gmail.com>
2023-11-24 12:20:58 +0100

    Refactor data context providers in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import { DataContextProvider } from "./context/DataValueProvider";
+import { DataValueProvider } from "./context/DataValueProvider";
 import { useProjects } from "./lib/react-query/queries";
 import { DataParamsProvider } from "./context/DataParamsProvider";
 
@@ -12,35 +12,35 @@ function App() {
   const location = useLocation();
 
   return (
-    <div
-      className={
-        "container flex items-center justify-center mx-auto my-auto h-screen"
-      }
-    >
-      <div className={"bg-white rounded-md"}>
-        <Header />
-        <div className="border-t">
-          <div className="">
-            <div className="flex">
-              <Sidebar buildings={buildings ?? []} isLoading={isLoading} />
-              <div className="col-span-3 lg:col-span-4 lg:border-l">
-                <div className="h-full px-4 py-6 lg:px-8">
-                  {location.pathname === "/" ? (
-                    <div>To define</div>
-                  ) : (
-                    <DataParamsProvider>
-                      <DataContextProvider>
+    <DataParamsProvider>
+      <DataValueProvider>
+        <div
+          className={
+            "container flex items-center justify-center mx-auto my-auto h-screen"
+          }
+        >
+          <div className={"bg-white rounded-md"}>
+            <Header />
+            <div className="border-t">
+              <div className="">
+                <div className="flex">
+                  <Sidebar buildings={buildings ?? []} isLoading={isLoading} />
+                  <div className="col-span-3 lg:col-span-4 lg:border-l">
+                    <div className="h-full px-4 py-6 lg:px-8">
+                      {location.pathname === "/" ? (
+                        <div>To define</div>
+                      ) : (
                         <Outlet />
-                      </DataContextProvider>
-                    </DataParamsProvider>
-                  )}
+                      )}
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </DataValueProvider>
+    </DataParamsProvider>
   );
 }
 
