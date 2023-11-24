0ebbd1707bd4ff45be615521febad0897fde28b6
Maxime Organi <maxime.organi@gmail.com>
2023-11-24 17:36:03 +0100

    Refactor DateSelector component

--- src/components/DateSelector.tsx
+++ src/components/DateSelector.tsx
@@ -4,7 +4,6 @@ import { useDataContext } from "@/context/DataValueProvider";
 import { useDateRange } from "@/hooks/useDateRange";
 import { useTemporalAggregation } from "@/hooks/useTemporalAggregation";
 import { useProjects } from "@/lib/react-query/queries";
-import { SameDateRangeCheckbox } from "./SameDateRangeCheckbox";
 
 export const DateSelector = () => {
   const [selectedDateRange, setDateRangeFilter] = useDateRange();
@@ -13,16 +12,13 @@ export const DateSelector = () => {
   const { currentBuilding } = useProjects();
 
   return (
-    <div>
-      <DatePickerWithRange
-        selectableDateRange={selectableDateRange}
-        selectedDateRange={selectedDateRange || selectableDateRange}
-        onChange={setDateRangeFilter}
-        isLoading={isLoading}
-        temporalAggregation={temporalAggregation}
-        timezone={currentBuilding?.timezone}
-      />
-      <SameDateRangeCheckbox />
-    </div>
+    <DatePickerWithRange
+      selectableDateRange={selectableDateRange}
+      selectedDateRange={selectedDateRange || selectableDateRange}
+      onChange={setDateRangeFilter}
+      isLoading={isLoading}
+      temporalAggregation={temporalAggregation}
+      timezone={currentBuilding?.timezone}
+    />
   );
 };
