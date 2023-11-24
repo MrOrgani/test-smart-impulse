2eff312d16dcf460768ce4da0cb8023ea4114de6
Maxime Organi <maxime.organi@gmail.com>
2023-11-24 13:16:26 +0100

    Refactor data parameters context and provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,39 @@ import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 import { DataValueProvider } from "./context/DataValueProvider";
 import { useProjects } from "./lib/react-query/queries";
-import { DataParamsProvider } from "./context/DataParamsProvider";
 
 function App() {
   const { data: buildings, isLoading } = useProjects();
   const location = useLocation();
 
   return (
-    <DataParamsProvider>
-      <DataValueProvider>
-        <div
-          className={
-            "container flex items-center justify-center mx-auto my-auto h-screen"
-          }
-        >
-          <div className={"bg-white rounded-md"}>
-            <Header />
-            <div className="border-t">
-              <div className="">
-                <div className="flex">
-                  <Sidebar buildings={buildings ?? []} isLoading={isLoading} />
-                  <div className="col-span-3 lg:col-span-4 lg:border-l">
-                    <div className="h-full px-4 py-6 lg:px-8">
-                      {location.pathname === "/" ? (
-                        <div>To define</div>
-                      ) : (
-                        <Outlet />
-                      )}
-                    </div>
-                  </div>
+    <div
+      className={
+        "container flex items-center justify-center mx-auto my-auto h-screen"
+      }
+    >
+      <div className={"bg-white rounded-md"}>
+        <Header />
+        <div className="border-t">
+          <div className="">
+            <div className="flex">
+              <Sidebar buildings={buildings ?? []} isLoading={isLoading} />
+              <div className="col-span-3 lg:col-span-4 lg:border-l">
+                <div className="h-full px-4 py-6 lg:px-8">
+                  {location.pathname === "/" ? (
+                    <div>To define</div>
+                  ) : (
+                    <DataValueProvider>
+                      <Outlet />
+                    </DataValueProvider>
+                  )}
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </DataValueProvider>
-    </DataParamsProvider>
+      </div>
+    </div>
   );
 }
 
