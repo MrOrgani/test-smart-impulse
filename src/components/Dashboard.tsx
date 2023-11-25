c53bdff03e4ef7668a0e183ce8d66f5df757b403
Maxime Organi <maxime.organi@gmail.com>
2023-11-25 10:23:48 +0100

    Refactor Dashboard component and add new widgets

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,26 +1,59 @@
 import React from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import { StackedBarChart } from "./Charts/StackedBarChart";
+import { Card, CardContent, CardTitle } from "./ui/card";
+import { StackedBarChart } from "./Charts/StackedBarChart/StackedBarChart";
 import { DateSelector } from "./DateSelector";
 import { TemporalAggregationSelector } from "./TemporalAggregationSelector";
 import { ResetButton } from "./ResetButton";
 import { MeasurementUnitSelector } from "./MeasurementUnitSelector";
+import { AmountSpentWidget } from "./Widgets/AmountSpentWidget";
+import { MostExpensiveCategoryWidget } from "./Widgets/MostExpensiveCategoryWidget";
+import { useTemporalAggregation } from "@/hooks/useTemporalAggregation";
+import { MostExpensiveTemporalAggregationPerCategory } from "./Widgets/MostExpensiveTemporalAggregationPerCategory";
 
 export const Dashboard = () => {
+  const [temporalAggregation] = useTemporalAggregation();
   return (
     <div>
-      <Card className="col-span-4">
-        <CardHeader>
-          <CardTitle className="flex  items-baseline">
-            Consumption in <MeasurementUnitSelector />
+      <Card className="">
+        <CardContent className="flex items-center">
+          Consumption in <MeasurementUnitSelector />
+          <DateSelector />
+          <TemporalAggregationSelector />
+          <ResetButton />
+        </CardContent>
+      </Card>
+      <Card className="grid grid-cols-2">
+        <Card className="grid grid-cols-2">
+          <Card className="flex flex-col">
+            <CardTitle className="items-baseline text-xs text-gray-500">
+              Amount spent
+            </CardTitle>
+            <CardContent className="mt-auto">
+              <AmountSpentWidget />
+            </CardContent>
+          </Card>
+          <Card className="flex flex-col">
+            <CardTitle className="items-baseline text-xs text-gray-500">
+              Most expensive category
+            </CardTitle>
+            <CardContent className="mt-auto">
+              <MostExpensiveCategoryWidget />
+            </CardContent>
+          </Card>
+          <Card className="flex flex-col"></Card>
+          <Card className="flex flex-col"></Card>
+        </Card>
+        <Card className="flex flex-col">
+          <CardTitle className="items-baseline text-xs text-gray-500">
+            Most expensive {temporalAggregation} per category
           </CardTitle>
-        </CardHeader>
-        <CardContent className="pl-2 ">
-          <div className="flex gap-2">
-            <DateSelector />
-            <TemporalAggregationSelector />
-            <ResetButton />
-          </div>
+          <CardContent className="mt-auto">
+            <MostExpensiveTemporalAggregationPerCategory />
+          </CardContent>
+        </Card>
+      </Card>
+      <Card className="col-span-4">
+        <CardContent className=" ">
           <StackedBarChart />
         </CardContent>
       </Card>
