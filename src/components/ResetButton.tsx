f43bcd57f84ac39230d81602358cb1ed4eb62cf9
Maxime Organi <maxime.organi@gmail.com>
2023-11-26 21:06:16 +0100

    Refactor ResetButton component to update URLSearchParams

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -2,17 +2,28 @@ import React from "react";
 import { Button } from "./ui/button";
 import { useDataContext } from "@/context/DataValueProvider";
 import { RotateCw } from "lucide-react";
-import { useDateRange } from "@/hooks/useDateRange";
+import { useSearchParams } from "react-router-dom";
 
-export const ResetButton = () => {
-  const [, setDateRangeFilter] = useDateRange();
+export const ResetButton: React.FC = () => {
   const { selectableDateRange } = useDataContext();
+  const [, setURLSearchParams] = useSearchParams();
+
   return (
     <Button
       variant={"outline"}
       aria-label="Reset date range filter"
       className="bg-white p-1 lg:ml-auto"
-      onClick={() => setDateRangeFilter(selectableDateRange)}
+      onClick={() => {
+        setURLSearchParams({
+          dateRange: encodeURI(
+            `${
+              selectableDateRange?.from?.toISOString() ?? ""
+            }_${selectableDateRange?.to?.toISOString()}`,
+          ),
+          measureUnit: "MWh",
+          temporalAggregation: "day",
+        });
+      }}
     >
       <RotateCw />
     </Button>
