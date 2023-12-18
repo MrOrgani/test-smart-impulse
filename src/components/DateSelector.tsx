5406d0ab1375543745c13ca1dc9baa6f4c78de24
Maxime Organi <maxime.organi@gmail.com>
2023-12-18 15:53:50 +0100

    SelectableDateRange is now defined inside DateSelector, by using fetchedData

--- src/components/DateSelector.tsx
+++ src/components/DateSelector.tsx
@@ -1,20 +1,45 @@
-import React from "react";
-import { DatePickerWithRange } from "./DateRangePicker";
-import { useDataContext } from "@/context/DataValueProvider";
-import { useDateRange } from "@/hooks/useDateRange";
-import { useTemporalAggregation } from "@/hooks/useTemporalAggregation";
-import { useProjects } from "@/lib/react-query/queries";
+import React from 'react';
+import { DatePickerWithRange } from './DateRangePicker';
+import { useDataContext } from '@/context/DataValueProvider';
+import { useDateRange } from '@/hooks/useDateRange';
+import { useTemporalAggregation } from '@/hooks/useTemporalAggregation';
+import { useProjects } from '@/lib/react-query/queries';
+import { DateRange } from 'react-day-picker';
+import * as d3 from 'd3';
 
 export const DateSelector = () => {
-  const [selectedDateRange, setDateRangeFilter] = useDateRange();
+  const [selectedDateRangeAsString, setDateRangeFilter] = useDateRange();
   const [temporalAggregation] = useTemporalAggregation();
-  const { selectableDateRange, isLoading } = useDataContext();
+  const { data: fetchedData, isLoading } = useDataContext();
   const { currentBuilding } = useProjects();
+
+  const [from, to] = selectedDateRangeAsString?.split('_') ?? [];
+
+  // get the longest data array property in the fetchedData array
+  const longestDataArray = fetchedData.reduce((acc, curr) => {
+    return curr.data.length > acc.data.length ? curr : acc;
+  }, fetchedData[0]);
+  const [selectableFrom, selectableTo] = longestDataArray?.data
+    ? d3.extent(longestDataArray?.data, (d) => d[0])
+    : [];
+
+  const selectedDateRange = selectedDateRangeAsString
+    ? ({
+        from: new Date(from),
+        to: new Date(to),
+      } as DateRange)
+    : selectableFrom && selectableTo
+      ? { from: new Date(selectableFrom), to: new Date(selectableTo) }
+      : undefined;
 
   return (
     <DatePickerWithRange
-      selectableDateRange={selectableDateRange}
-      selectedDateRange={selectedDateRange || selectableDateRange}
+      selectableDateRange={
+        selectableFrom && selectableTo
+          ? { from: new Date(selectableFrom), to: new Date(selectableTo) }
+          : undefined
+      }
+      selectedDateRange={selectedDateRange}
       onChange={setDateRangeFilter}
       isLoading={isLoading}
       temporalAggregation={temporalAggregation}
