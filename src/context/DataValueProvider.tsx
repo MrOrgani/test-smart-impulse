3e354e85886ee092e305e34daed22a94f876e7a8
Maxime Organi <maxime.organi@gmail.com>
2023-12-18 16:34:59 +0100

    Data provider only provides fetched data

--- src/context/DataValueProvider.tsx
+++ src/context/DataValueProvider.tsx
@@ -1,22 +1,16 @@
-import React from "react";
-import { formatDatasets } from "@/utils/utils";
-import { getTimeLabels } from "@/utils/getTimeLabels";
-import { DateRange } from "react-day-picker";
-import * as d3 from "d3";
-import dayjs from "dayjs";
-import { useEnergyConsumption, useProjects } from "@/lib/react-query/queries";
-import { useTemporalAggregation } from "@/hooks/useTemporalAggregation";
-import { useDateRange } from "@/hooks/useDateRange";
-import { BasicFormattedDatasets } from "@/lib/types";
+import React, { useMemo } from 'react';
+
+import { useEnergyConsumption, useProjects } from '@/lib/react-query/queries';
+import type { FetchedDataSet } from '@/lib/types';
 
 const DataContext = React.createContext<{
-  data: { datasets: BasicFormattedDatasets; labels: string[] };
-  selectableDateRange: DateRange | undefined;
+  data: FetchedDataSet[];
   isLoading: boolean;
+  isFetching: boolean;
 }>({
-  data: { datasets: [], labels: [] },
-  selectableDateRange: undefined,
+  data: [],
   isLoading: true,
+  isFetching: true,
 });
 
 export const DataValueProvider: React.FC<{
@@ -26,52 +20,25 @@ export const DataValueProvider: React.FC<{
   const { data: fetchedData, ...restFetchedData } = useEnergyConsumption(
     currentBuilding?.uuid,
   );
-  const [selectedTemporalAggregation] = useTemporalAggregation();
-  const [selectedDateRange] = useDateRange();
 
-  const labels = getTimeLabels(
-    fetchedData ?? [],
-    selectedTemporalAggregation,
-    currentBuilding?.timezone ?? "Europe/Paris",
-  );
+  const value = useMemo(() => {
+    return {
+      data: fetchedData ?? [],
+      ...restFetchedData,
+      isLoading:
+        restFetchedData.isLoading ||
+        restFetchedData.isFetching ||
+        !currentBuilding,
+    };
+  }, [currentBuilding, fetchedData, restFetchedData]);
 
-  const basicDatasets = formatDatasets(
-    fetchedData ?? [],
-    selectedDateRange,
-    selectedTemporalAggregation,
-    currentBuilding?.timezone ?? "Europe/Paris",
-  );
-
-  const selectableDateExtendArray = d3.extent(labels);
-  const selectableDateRange =
-    labels.length > 0
-      ? {
-          from: dayjs(selectableDateExtendArray[0]).toDate(),
-          to: dayjs(selectableDateExtendArray[1]).toDate(),
-        }
-      : undefined;
-
-  return (
-    <DataContext.Provider
-      value={{
-        data: {
-          labels,
-          datasets: basicDatasets,
-        },
-        selectableDateRange,
-        ...restFetchedData,
-      }}
-    >
-      {children}
-    </DataContext.Provider>
-  );
+  return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
 };
 
 export const useDataContext = () => {
-  const { data, selectableDateRange, ...rest } = React.useContext(DataContext);
+  const { data, ...rest } = React.useContext(DataContext);
   return {
     data,
-    selectableDateRange,
     ...rest,
   };
 };
