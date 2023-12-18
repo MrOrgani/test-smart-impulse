dbddb0d72546f4e2878fffa303f824d0f7769d4d
Maxime Organi <maxime.organi@gmail.com>
2023-12-18 14:08:36 +0100

    single quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import { Outlet, useLocation } from "react-router-dom";
-import { Header } from "./components/Header";
-import { Sidebar } from "./components/Sidebar";
-import { DataValueProvider } from "./context/DataValueProvider";
-import { useProjects } from "./lib/react-query/queries";
+import { Outlet, useLocation } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { DataValueProvider } from './context/DataValueProvider';
+import { useProjects } from './lib/react-query/queries';
 
 export const App: React.FC = () => {
   const { data: buildings, isLoading } = useProjects();
@@ -13,17 +13,17 @@ export const App: React.FC = () => {
   return (
     <div
       className={
-        "flex-col xl:flex xl:flex-row items-center justify-center w-screen p-2 h-screen"
+        'flex-col xl:flex xl:flex-row items-center justify-center w-screen p-2 h-screen'
       }
     >
-      <div className={"bg-white rounded-md"}>
+      <div className={'bg-white rounded-md'}>
         <Header />
         <div className="border-t">
           <div className="flex flex-col xl:flex xl:flex-row">
             <Sidebar buildings={buildings ?? []} isLoading={isLoading} />
             <div className="col-span-3 xl:col-span-4 xl:border-l">
               <div className="xl:w-[1000px] h-full xl:h-[780px] p-2 xl:px-8">
-                {location.pathname === "/" ? (
+                {location.pathname === '/' ? (
                   <div className="flex h-full flex-col items-center justify-center">
                     <h1 className="text-2xl font-bold tracking-tight">
                       Welcome,
