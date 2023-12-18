35d6d1d1c9fe7f28b5fb598a213f9c868cd588a5
Maxime Organi <maxime.organi@gmail.com>
2023-12-18 14:13:49 +0100

    Add useWebWorker hook for processing web workers

---
+++ src/hooks/useWebWorker.tsx
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useState } from "react";
+
+export interface IBaseWorkerResponse<T> {
+  result: T;
+  error?: any;
+}
+
+export const useWebWorker = <TResult, TWorkerPayload>(worker: Worker) => {
+  const [running, setRunning] = useState(false);
+  const [error, setError] = useState<any>();
+  const [result, setResult] = useState<TResult>();
+
+  const startProcessing = useCallback(
+    (data: TWorkerPayload) => {
+      setRunning(true);
+      worker.postMessage(data);
+    },
+    [worker],
+  );
+
+  useEffect(() => {
+    const onMessage = (event: MessageEvent<IBaseWorkerResponse<TResult>>) => {
+      setError(event.data.error);
+      setResult(event.data.result);
+      setRunning(false);
+    };
+    worker.addEventListener("message", onMessage);
+    return () => {
+      worker.removeEventListener("message", onMessage);
+    };
+  }, [worker]);
+
+  return {
+    startProcessing,
+    running,
+    error,
+    result,
+  };
+};
