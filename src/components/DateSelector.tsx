46cc036abf638c4d3d0b181d57c8b08d13eaa946
Maxime Organi <maxime.organi@gmail.com>
2023-12-18 16:32:59 +0100

    Refactor DateSelector component to use react-day-picker and update date range logic

--- src/components/DateSelector.tsx
+++ src/components/DateSelector.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { DatePickerWithRange } from './DateRangePicker';
+import { DateRange } from 'react-day-picker';
+
 import { useDataContext } from '@/context/DataValueProvider';
 import { useDateRange } from '@/hooks/useDateRange';
 import { useTemporalAggregation } from '@/hooks/useTemporalAggregation';
 import { useProjects } from '@/lib/react-query/queries';
-import { DateRange } from 'react-day-picker';
-import * as d3 from 'd3';
+import { getExtendedDateFromDatasets } from '@/utils/getExtendedDateFromDatasets';
+
+import { DatePickerWithRange } from './DateRangePicker';
 
 export const DateSelector = () => {
   const [selectedDateRangeAsString, setDateRangeFilter] = useDateRange();
@@ -16,27 +18,25 @@ export const DateSelector = () => {
   const [from, to] = selectedDateRangeAsString?.split('_') ?? [];
 
   // get the longest data array property in the fetchedData array
-  const longestDataArray = fetchedData.reduce((acc, curr) => {
-    return curr.data.length > acc.data.length ? curr : acc;
-  }, fetchedData[0]);
-  const [selectableFrom, selectableTo] = longestDataArray?.data
-    ? d3.extent(longestDataArray?.data, (d) => d[0])
-    : [];
-
+  const [selectableDateStart, selectableDateEnd] =
+    getExtendedDateFromDatasets(fetchedData);
   const selectedDateRange = selectedDateRangeAsString
     ? ({
         from: new Date(from),
         to: new Date(to),
       } as DateRange)
-    : selectableFrom && selectableTo
-      ? { from: new Date(selectableFrom), to: new Date(selectableTo) }
+    : selectableDateStart && selectableDateEnd
+      ? { from: new Date(selectableDateStart), to: new Date(selectableDateEnd) }
       : undefined;
 
   return (
     <DatePickerWithRange
       selectableDateRange={
-        selectableFrom && selectableTo
-          ? { from: new Date(selectableFrom), to: new Date(selectableTo) }
+        selectableDateStart && selectableDateEnd
+          ? {
+              from: new Date(selectableDateStart),
+              to: new Date(selectableDateEnd),
+            }
           : undefined
       }
       selectedDateRange={selectedDateRange}
