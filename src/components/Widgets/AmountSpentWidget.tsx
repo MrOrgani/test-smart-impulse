720523f72d2da3b34bd4353472aaea9e16233a02
Maxime Organi <maxime.organi@gmail.com>
2023-12-18 15:55:12 +0100

    Better skeleton for AmountSpentWidget + display 0 euros when no data

--- src/components/Widgets/AmountSpentWidget.tsx
+++ src/components/Widgets/AmountSpentWidget.tsx
@@ -1,25 +1,26 @@
-import React from "react";
-import { useDataContext } from "@/context/DataValueProvider";
-import { getValueModifier } from "@/utils/getValueModifier";
-import { Skeleton } from "../ui/skeleton";
+import React from 'react';
+import { useDataContext } from '@/context/DataValueProvider';
+import { getValueModifier } from '@/utils/getValueModifier';
+import { Skeleton } from '../ui/skeleton';
 
 export const AmountSpentWidget: React.FC = () => {
   const { data, isLoading } = useDataContext();
 
   if (isLoading) {
-    return <Skeleton className="w-24 h-6" />;
+    return (
+      <div className="grid grid-cols-2">
+        <Skeleton className="w-24 h-6" />
+        <Skeleton className="ml-auto w-24 h-6" />
+      </div>
+    );
   }
 
-  const totalAmountSpent = data.datasets
-    ?.find((dataset) => dataset.label === "Energie totale")
-    ?.tooltip.reduce((acc, value) => acc + value[1], 0);
+  const totalAmountSpent = data
+    ?.find((dataset) => dataset.label === 'Energie totale')
+    ?.data.reduce((acc, value) => acc + value[1], 0);
 
-  if (totalAmountSpent === undefined) {
-    return null;
-  }
-
-  const valueModifier = getValueModifier("euros");
-  const amount = valueModifier(totalAmountSpent).toFixed(2);
+  const valueModifier = getValueModifier('euros');
+  const amount = valueModifier(totalAmountSpent ?? 0).toFixed(2);
 
   return (
     <div className="grid grid-cols-2">
