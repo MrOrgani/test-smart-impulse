61b4c704a90f421cf32b60331a23fb8ac8b20e53
Maxime Organi <maxime.organi@gmail.com>
2023-12-20 19:54:38 +0100

    Refactor applyDateRangeFilter function

--- src/utils/applyDateRangeFilter.ts
+++ src/utils/applyDateRangeFilter.ts
@@ -1,44 +1,45 @@
-import type { BasicFormattedDataset } from "../lib/types";
-import type { DateRange } from "react-day-picker";
+import type { DateRange } from 'react-day-picker';
+
+import type { BasicFormattedDataset } from '../lib/types';
 
 export const applyDateRangeFilter = (
   datasets: BasicFormattedDataset[],
-  dateRange: DateRange | undefined,
-): BasicFormattedDataset[] => {
+  dateRange: DateRange,
+) => {
   if (dateRange?.from === undefined || dateRange?.to === undefined) {
     return datasets;
   }
 
   const { from, to } = dateRange;
-
   const startFilterRangeTimestamp = from.getTime();
   const endFilterRangeTimestamp = to.getTime();
 
-  const filteredDataSets = datasets.map((dataset) => {
-    const filteredData = dataset.data?.map<[EpochTimeStamp, number]>((item) => {
-      if (!Array.isArray(item)) return [0, 0];
+  const filteredDataSets = [];
+
+  for (let i = 0; i < datasets.length; i++) {
+    const dataset = datasets[i];
+
+    for (let j = 0; j < dataset.data.length; j++) {
+      const item = dataset.data[j];
+      if (!Array.isArray(item)) {
+        dataset['data'][j] = [0, 0];
+        dataset['tooltip'][j] = [0, 0];
+        continue;
+      }
       const timestamp = item[0];
-      return startFilterRangeTimestamp <= timestamp &&
+      if (
+        startFilterRangeTimestamp <= timestamp &&
         timestamp <= endFilterRangeTimestamp
-        ? item
-        : [timestamp, 0];
-    });
-    const filteredTooltips = dataset.tooltip?.map<[EpochTimeStamp, number]>(
-      (item) => {
-        if (!Array.isArray(item)) return [0, 0];
-        const timestamp = item[0];
-        return startFilterRangeTimestamp <= timestamp &&
-          timestamp <= endFilterRangeTimestamp
-          ? item
-          : [timestamp, 0];
-      },
-    );
-    return {
-      ...dataset,
-      data: filteredData,
-      tooltip: filteredTooltips,
-    };
-  });
+      ) {
+        continue;
+      } else {
+        dataset['data'][j] = [timestamp, 0];
+        dataset['tooltip'][j] = [timestamp, 0];
+      }
+    }
+
+    filteredDataSets.push(dataset);
+  }
 
   return filteredDataSets;
 };
