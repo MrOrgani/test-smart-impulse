aaaa6abd524573e579deebea225f6a3a5fadad40
Maxime Organi <maxime.organi@gmail.com>
2023-12-21 12:08:59 +0100

    adding webworker types

--- src/utils/formatScript.ts
+++ src/utils/formatScript.ts
@@ -1,5 +1,22 @@
+import { DateRange } from 'react-day-picker';
+
+import { AggregatedDataset, FetchedDataset } from '@/lib/types';
+
 import { aggregateDatasets } from './aggregateDatasets';
 import { applyDateRangeFilter } from './applyDateRangeFilter';
+
+export type IWorkerResult = {
+  basicDatasets: AggregatedDataset[];
+  buildingId: string;
+};
+
+export type IWorkerRequest = {
+  datasets: FetchedDataset[];
+  temporalAggregation: string;
+  timezone: string;
+  buildingId: string;
+  dateRange: DateRange;
+};
 
 onmessage = (e) => {
   const { datasets, temporalAggregation, timezone, buildingId, dateRange } =
