3925db59faad65fd9af783f40725c25834749707
Maxime Organi <maxime.organi@gmail.com>
2023-12-21 18:38:54 +0100

    Refactor getExtendedDateFromDatasets function to handle AggregatedDataset

--- src/utils/getExtendedDateFromDatasets.ts
+++ src/utils/getExtendedDateFromDatasets.ts
@@ -1,15 +1,25 @@
 import * as d3 from 'd3';
 
-import { FetchedDataset } from '@/lib/types';
+import { AggregatedDataset, FetchedDataset } from '@/lib/types';
 
-export const getExtendedDateFromDatasets = (datasets: FetchedDataset[]) => {
-  if (!datasets.length) return [];
+export const getExtendedDateFromDatasets = (
+  datasets: FetchedDataset[] | AggregatedDataset[] | undefined,
+) => {
+  if (!datasets?.length) return [];
   const longestDataArray = datasets.reduce((acc, curr) => {
     return curr.data.length > acc.data.length ? curr : acc;
   }, datasets[0]);
-  const [selectableFrom, selectableTo] = longestDataArray?.data
-    ? d3.extent(longestDataArray?.data, (d) => d[0])
-    : [];
 
-  return [selectableFrom, selectableTo];
+  if (typeof longestDataArray?.data[0][0] === 'number') {
+    const [selectableFrom, selectableTo] = longestDataArray?.data
+      ? d3.extent(longestDataArray?.data as [number, number][], (d) => d[0])
+      : [];
+    return [selectableFrom, selectableTo];
+  }
+
+  const aggregatedData = longestDataArray.data;
+
+  const selectableDateStart = aggregatedData[0][0];
+  const selectableDateEnd = aggregatedData[aggregatedData.length - 1][0];
+  return [selectableDateStart, selectableDateEnd];
 };
